726e31a1610b61ea3a577703e56e2cb3b775c0e9
LucaNotaro <lucakirk9554@gmail.com>
2022-04-22 17:57:56 +0200

    Aggiungere i file di progetto.

---
+++ Events/GestoreEventi.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class GestoreEventi
+    {
+
+        public DateTime dataOra;
+        private string nome;
+        private int postitotali;
+        private int postidaprenotare;
+        public int postifiniti;
+        public int check;
+        
+        
+
+        public GestoreEventi(string nome1, DateTime date2, int postitotali1)
+        {
+
+
+
+
+
+           
+
+                setNome(nome1);
+                setDataOra(date2);
+               
+
+
+
+
+
+
+
+
+
+            getPostitot();
+            getPostitot();
+            this.postitotali = postitotali1;
+            this.postifiniti = postifiniti;
+            this.check = check;
+            
+            
+
+          
+
+
+
+
+
+
+        }
+
+      
+
+       
+
+        public void Prenotaposti(int b)
+        {
+
+
+
+            this.postidaprenotare = b;
+            this.postifiniti = postitotali - postidaprenotare;
+
+
+           
+            
+
+            
+
+
+
+        }
+
+        public void setPostitott(int postitotali1)
+
+        {
+
+            if (postitotali1 < 0)
+            {
+
+                throw new ArgumentOutOfRangeException();
+
+            }
+
+            this.postitotali = postitotali1;
+
+
+        }
+
+       
+        public void Disdici(int p)
+        {
+            check=postidaprenotare;
+            int flag = 1;
+            
+            
+
+
+                if (p <= postidaprenotare)
+                {
+                    this.postidaprenotare = postidaprenotare - p;
+                    flag = 0;
+
+                }
+
+                else
+                {
+
+
+                    Console.WriteLine("posti non a sufficienza,riprovare");
+                    Console.WriteLine("quanti posti vuoi disdire?");
+                }
+
+
+            
+           
+
+
+
+
+
+
+
+
+
+
+        }
+        public int getPostidapren()
+        {
+
+            return postidaprenotare;
+            
+
+           
+
+        }
+
+
+        
+
+        public int getPostitot()
+        {
+
+           
+            return postitotali;
+
+
+
+        }
+
+        public void setNome (string nome1)
+        {
+
+            if (nome1 == "ciao")
+            {
+
+                throw new ArgumentNullException(nameof(nome1));
+            }
+
+            else
+            {
+
+                this.nome = nome1;
+
+            }
+            
+
+            
+
+
+            }
+
+        public void setDataOra(DateTime date2)
+        {
+
+
+            this.dataOra = date2;
+
+            if (date2<DateTime.Now)
+            {
+                throw new InvalidCastException();
+
+            }
+
+          
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        override
+        public string ToString() => "nome " + nome + "   posti1 " + postitotali + "   posti2 " + postidaprenotare + " posti" + postifiniti;
+
+
+    }
+
+
+
+
+
+    }
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+         
+
---
+++ Events/Program.cs
@@ -0,0 +1,106 @@
+﻿using Events;
+
+int postitotali;
+int c;
+string nome_evento;
+DateTime data;
+
+try
+{
+
+
+
+
+    Console.Write("inserisci il nome dell'evento: ");
+    nome_evento = Console.ReadLine();
+    Console.Write("inserisci la data dell'evento: ");
+    data = DateTime.Parse(Console.ReadLine());
+    Console.Write("inserisci il numero dei posti totali ");
+    postitotali = int.Parse(Console.ReadLine());
+
+
+    Console.Write("quanti posti desideri prenotare? ");
+
+    c = int.Parse(Console.ReadLine());
+
+
+
+
+GestoreEventi app = new GestoreEventi(nome_evento,data,postitotali);
+
+
+app.Prenotaposti(c);
+
+
+
+
+
+
+Console.WriteLine(app.ToString());
+
+int t;
+
+
+int flag2 = 1 ;
+do
+{
+    
+Console.WriteLine("vuoi disdire i posti?si/no");
+
+
+    string s = Console.ReadLine();
+
+
+
+    if (s == "si")
+    {
+        
+        t = int.Parse(Console.ReadLine());
+        app.Disdici(t);
+        
+
+        if (t == app.check)
+        {
+
+
+            flag2 = 1;
+
+        }
+        Console.WriteLine(app.ToString());
+
+    }
+
+
+    if (s == "no")
+    {
+
+
+        flag2 = 0;
+
+    }
+
+
+
+
+}
+
+while (flag2 ==1);
+
+}
+
+                
+catch(ArgumentException e)
+{
+    Console.WriteLine("errore di inserimento" + e.ParamName);
+
+
+
+}
+catch (InvalidCastException)
+{
+
+    Console.WriteLine("data errata");
+
+
+}
+
